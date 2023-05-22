af85695a8802e398678629ffc4ec5cb2a18345ae
Egg55 <emilianomilanog@gmail.com>
2023-05-21 19:11:52 -0600

    added more to submit_and_test_guess function
    Made game playable

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
 use rand::seq::IteratorRandom;
 use std::{
     fs::File,
-    io::{BufRead, BufReader, self, Read}
+    io::{self, BufRead, BufReader, Read},
 };
 
 const FILENAME: &str = "words.txt";
 
-struct Wordle { 
+struct Wordle {
     word: String,
     bad_chars: Vec<char>,
-    good_chars: Vec<char>
+    good_chars: Vec<char>,
 }
 
 impl Wordle {
@@ -21,22 +21,44 @@ impl Wordle {
         }
     }
 
-    fn submit_and_test_guess(&self,guess: String) {
+    fn submit_and_test_guess(&mut self, guess: String) {
         let guess: Vec<char> = guess.chars().collect();
 
         let word: Vec<char> = self.word.chars().collect();
 
         let matching_letters = compare_vec(&guess, &word);
-        println!("{:?}",matching_letters)
+
+        println!("{:?}", matching_letters);
+
+        matching_letters.iter().enumerate().for_each(|(i, b)| {
+            let letter = guess[i];
+            match b {
+                true => {
+                    if !self.good_chars.contains(&letter) {
+                        self.good_chars.push(letter)
+                    }
+                }
+                false => {
+                    if !self.bad_chars.contains(&letter) {
+                        self.bad_chars.push(letter)
+                    }
+                }
+            }
+        });
+
+        println!("Good: {:?}\nBad: {:?}",self.good_chars,self.bad_chars);
+        
+        if !matching_letters.contains(&false) {
+            println!("you win!")
+        }
     }
 
     fn new_random_word(&mut self) {
         self.word = get_random_line_from_file(FILENAME);
-
     }
 }
 
-fn compare_vec<T: std::cmp::PartialEq >(a: &Vec<T>, b: &Vec<T>) -> Vec<bool> {
+fn compare_vec<T: std::cmp::PartialEq>(a: &Vec<T>, b: &Vec<T>) -> Vec<bool> {
     a.iter()
         .zip(b.iter())
         .map(|(x, y)| x == y)
@@ -44,8 +66,8 @@ fn compare_vec<T: std::cmp::PartialEq >(a: &Vec<T>, b: &Vec<T>) -> Vec<bool> {
 }
 
 fn get_random_line_from_file(filename: &str) -> String {
-    let f = File::open(filename)
-        .unwrap_or_else(|e| panic!("Error opening file {}: {}", FILENAME, e));
+    let f =
+        File::open(filename).unwrap_or_else(|e| panic!("Error opening file {}: {}", FILENAME, e));
     let f = BufReader::new(f);
 
     let lines = f.lines().map(|l| l.expect("Error reading line"));
@@ -56,14 +78,15 @@ fn get_random_line_from_file(filename: &str) -> String {
 }
 
 fn main() {
-    let wordle = Wordle::new(String::from(""));
+    let mut wordle = Wordle::new(String::from(""));
 
     loop {
         let mut guess = String::new();
-        io::stdin().read_line(&mut guess).expect("Failed to read input");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read input");
         let guess = String::from(guess.trim());
         wordle.submit_and_test_guess(guess);
+
     }
-
 }
-
